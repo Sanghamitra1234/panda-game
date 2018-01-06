2aa3d18e6400a3575b66da692e59aa4d6f9c43ea
Sanghamitra1234 <sanghota4567@gmail.com>
2018-01-06 20:54:06 +0530

    panda basic features

---
+++ Assets/scripts/camerafollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+
+    private Transform lookat;
+    Vector3 startOffset;
+    private Vector3 moveVector;
+
+    private float transition = 0.0f;
+    private float animationDuration = 2.0f;
+    private Vector3 animationOffset = new Vector3(0, 5, 5);
+
+	// Use this for initialization
+	void Start () {
+        lookat=GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookat.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = lookat.position + startOffset;
+
+        moveVector.x = 0;
+
+        if (transition > 1.0f)
+
+        {
+           transform.position = moveVector;
+        }
+        else
+        {
+            // start zoom in 
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+            transform.LookAt(lookat.position + Vector3.up);
+        }
+	}
+}
---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+    CharacterController controller;
+    Vector3 moveVector;
+    [SerializeField]
+    public float speed;
+    public float horizontalspeed = 2.0f;
+    public float jumpForce = 4.0f;
+    float verticalVelocity = 0.0f;
+    float gravity = 12.0f;
+    private float animationDuration = 2.0f;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //for first 2 sec, till the camera comes down, none of the key ll work
+
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        //x
+        moveVector.x = Input.GetAxisRaw("Horizontal")*horizontalspeed;
+
+        //y
+        moveVector.y = verticalVelocity;
+        if (Input.GetKeyDown("space"))
+        {
+            controller.GetComponent<Animator>().SetTrigger("jump");
+            moveVector.y = jumpForce;
+        }
+
+        //forward
+        moveVector.z= speed;
+
+        controller.Move((moveVector * speed )*Time.deltaTime);
+	}
+}
